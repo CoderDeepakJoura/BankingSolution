5fbd8b28020889ef4aa7a1e22b7fc7cb10edda17
Deepak Joura <deepsaab745@gmail.com>
2025-07-17 22:18:59 +0530

    test written

--- backend/BankingPlatform.Tests/UnitTest1.cs
+++ backend/BankingPlatform.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
-﻿namespace BankingPlatform.Tests;
+﻿using System.Diagnostics;
+
+namespace BankingPlatform.Tests;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        Console.WriteLine("This is a test method in the BankingPlatform.Tests project.");
     }
 }
